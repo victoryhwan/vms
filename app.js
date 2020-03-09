21ea7e9768c30cfe6beda03b3c6328451227c15e
victoryhwan <victoryhwan7@gmail.com>
2020-03-10 00:51:48 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
+var http = require('http');
+var app = express();
+require('dotenv').config();
+app.set('port', process.env.PORT || 3030);
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
